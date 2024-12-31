6965f4b7a46c6b04177ea637b134abc7c072d2d6
Maik <maik@bybackfish.de>
2024-12-31 01:59:20 +0100

    Initial commit

---
+++ src/main/kotlin/de/bybackfish/schemaconverter/SchematicaConverter.kt
@@ -0,0 +1,14 @@
+package de.bybackfish.schemaconverter
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SchematicaConverter : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
